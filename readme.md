4481be9a2d241e064ff5cc96f39afa4a97b6e3d8
saraducks <saranyadhakshina25@gmail.com>
2017-05-16 20:08:40 -0700

    Refactoring code

---
+++ readme.md
@@ -0,0 +1,8 @@
+* create pyvenv dev environment
+pyvenv-3.4 sara-dev
+
+* activate pyvenv
+source sara-dev/bin/activate
+
+* now you should be inside sara-dev python virtualenv. Now pip install requirements
+pip install -r requirements.txt
---
+++ trie-cli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.4
+
+import trie
+import time
+
+t = trie.Trie()
+
+# measure the time taken for adding dictionary words to trienode
+start  = time.time()
+
+#open the local dictionary file and build the trie node
+local_dict = open('/usr/share/dict/words')
+for i in local_dict:
+    t.add_keyword(i)
+
+finish_import = time.time()
+
+# prints total time taken for opening dictionary file and creating tirenode
+print( "time for dictionary import %d seconds"%(finish_import - start))
+
+while(1):
+    # prompts for user input
+    print("enter query:")
+    query = input()
+    # measures the total time taken to search for prefix and return the result
+    start_query = time.time()
+    t.get_completions(query)
+    end_query = time.time()
+    print("time for search %d seconds" % (end_query - start_query))
