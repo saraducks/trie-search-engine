e275973a70914f1f4911c50368fd9b875826df93
saraducks <saranyadhakshina25@gmail.com>
2017-05-21 11:51:37 -0700

    added list

--- dictionary_webapp/trieflaskapp.py
+++ dictionary_webapp/trieflaskapp.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3.4
 
 from flask import Flask, render_template, jsonify, request
-
+import time
 import trie
 
 # application context
 app = Flask(__name__)
+trie_index = trie.Trie()
 
 @app.route("/")
 def hello():
@@ -14,15 +15,16 @@ def hello():
 @app.route('/search')
 def search():
     query = request.args.get('q')
+    start = time.time()
     result_list = []
     # validate query
     if query is not None and query is not "":
         # do the query
         #result will be the list of word completions
-        trie_index = trie.Trie()
         result_list = trie_index.get_completions(query)
+    end = time.time()
 
-    result = {"query": query, 'results':result_list}
+    result = {"query": query, 'results':result_list[:10], 'server_time':str(int(end-start))+"seconds"}
     return jsonify(result)
 
 if __name__ == '__main__':
